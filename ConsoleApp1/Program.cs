e22c9e98163855562c5e1fa8448c98d18f4346d4
Manh Nguyen <manh.nguyen@thirdsight.net>
2024-12-19 18:48:00 +0800

    Guide for Developer 01 Guide for Developer 02 Connection String

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using MyCompany.MyLib;
+using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace MyCompany.MyApp.MyConsole
+{
+    class Program
+    {
+        static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static readonly string _connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        private static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            Console.Write($"Connection String: {_connString}");
+
+            var arr = new int[] { 1, 2, 3 };
+            IMyClass myClass = new MyClass();
+            myClass.HasDuplicate(arr);
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Unit Testing with MSTest
 
-![Guide for Developer](docs/Unit_Testing.png)
+![Guide for Developer 01](docs/Unit_Testing.png)
+
+![Guide for Developer 02](docs/Unit_Testing_02.png)
+
+
+```
+Install-Package log4net
+Install-Package Newtonsoft.Json
+Install-Package Dapper
+Install-Package Dapper.Contrib
+Install-Package Dapper.SqlBuilder
+Install-Package Humanier
+Install-Package NodaTime
+Install-Package CsvHelper
+Install-Package Refit
+```
+
+# References
++ https://github.com/gtechsltn/DapperSelect
